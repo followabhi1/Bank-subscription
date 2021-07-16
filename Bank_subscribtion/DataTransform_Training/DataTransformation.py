a31e9d84d6c1519858b63614095c418224fcf945
followabhi1 <65127418+followabhi1@users.noreply.github.com>
2021-07-16 14:48:50 +0900

    Update DataTransformation.py

--- Bank_subscribtion/DataTransform_Training/DataTransformation.py
+++ Bank_subscribtion/DataTransform_Training/DataTransformation.py
@@ -9,7 +9,7 @@ class dataTransform:
      """
                This class shall be used for transforming the Good Raw Training Data before loading it in Database!!.
 
-               Written By: iNeuron Intelligence
+               Written By: AB
                Version: 1.0
                Revisions: None
 
